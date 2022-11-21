2a0c14f4f994ba8b703b6c02f99e1920140ed961
Jacob Harrison <88895714+JacobHarrison@users.noreply.github.com>
2022-11-21 05:43:21 -0600

    fundamental functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
-// check if this push and pull correctly
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
+    <div class="App">
+          <div class="header">
+              <pageHeader>Page Header Goes Here!</pageHeader>
+          </div>
+          <div class="databox">Data and Plots Go Here!</div>
+          <div class="item">
+              <label for="textEntry">Press button to update button's text with value of text field</label></div>
+          <div class="item">
+              <input type="text" id="tEntry" name="textEntry"></input></div>
+          <div class="item">
+              <button id="btn" onClick={clickText}>Off</button></div>
+          <div class="scroll">
+              random wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom wordsrandom words</div>
     </div>
   );
+}
+function clickText() {
+    var textValue = document.getElementById("tEntry").value;
+    document.getElementById("btn").textContent = textValue;
 }
 
 export default App;
