2f4a1940613801d6f91f8bf065e2023b22e6b478
Jacob Harrison <88895714+JacobHarrison@users.noreply.github.com>
2022-11-28 18:07:52 -0600

    page working
    no sql, but basic page functionality is made

---
+++ src/database.js
@@ -0,0 +1,18 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: 'localhost',
+    port: 3309,
+    user: 'yourusername',
+    password: 'yourpassword'
+});
+
+
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+    /*con.query("CREATE DATABASE mydb", function (err, result) {
+        if (err) throw err;
+        //console.log("Database created!");
+    });*/
+});
